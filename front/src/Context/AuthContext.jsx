f70a88fac74922fa75d2fbe401c221f0ab6ba8da
Sonu Kumar <sonukumar763303@gmail.com>
2025-07-06 11:20:18 +0530

    Update AuthContext.jsx

--- front/src/Context/AuthContext.jsx
+++ front/src/Context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  let serverUrl = "http://localhost:4000";
+  let serverUrl = "https://air-back.onrender.com";
   let [loading, setLoading] = useState(false);
   let value = {
     serverUrl,
